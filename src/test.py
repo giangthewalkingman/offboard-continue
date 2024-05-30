448ededd92ade8107112584628589240639a6c02
giangthewalkingman <giangsouleater@gmail.com>
2024-05-30 19:47:30 +0700

    afkjnfe

--- src/test.py
+++ src/test.py
@@ -7,7 +7,7 @@ address = 0x20
 
 bus = smbus.SMBus(1)
 while True:
-    bus.write_byte(address,0xEF)
+    bus.write_byte_data(address,0xA5,0x5A)
     time.sleep(0.5)
-    bus.write_byte(address,0xFF)
+    bus.write_byte_data(address,0xA5,0x5A)
 time.sleep(0.5)
